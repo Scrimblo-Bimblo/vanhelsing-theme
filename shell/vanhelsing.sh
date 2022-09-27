d3a9b9da014e3423b0af95dd1c14b1bbc333ded7
scrimblo <beebroox7@zohomail.com>
2022-09-27 16:12:00 -0600

    yaml

---
+++ shell/vanhelsing.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+/bin/echo -e "
+	\e]P0392525
+	\e]P1ff5555
+	\e]P2b4c544
+	\e]P3d79a1a
+	\e]P4a3d0c2
+	\e]P5f36daa
+	\e]P697da88
+	\e]P7caaaaa
+	\e]P8947777
+	\e]P9ff5555
+	\e]PAb4c544
+	\e]PBd79a1a
+	\e]PCa3d0c2
+	\e]PDf36daa
+	\e]PE97da88
+	\e]PFffdddd
+"
+
+clear
